26fd5cc80b09fd8873b15572dcf8727cb356754e
PratyutCS <pratyut987@gmail.com>
2024-01-08 12:17:58 +0530

    horizontal-scroll

--- file.js
+++ file.js
@@ -49,3 +49,24 @@ for (i = 0; i < slides.length; i++) {
 }
 slides[slideIndex-1].style.display = "flex";
 }
+
+
+const stickySections = [...document.querySelectorAll('.sticky_wrap')]
+
+window.addEventListener('scroll', (e) => {
+  for(let i = 0; i < stickySections.length; i++){
+    transform(stickySections[i])
+  }
+})
+
+function transform(section) {
+  const offsetTop = section.parentElement.offsetTop;
+  console.log("offsetTop   -   "+offsetTop);
+  const scrollSection = section.querySelector('.horizontal_scroll')
+  console.log(window.scrollY+" - "+window.innerHeight);
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+  console.log("percentage   -   "+percentage);
+  percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage;
+  console.log("percentage finalle   -   "+percentage);
+  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
+}
