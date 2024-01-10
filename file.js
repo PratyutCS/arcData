c673924f63f45c9a2bc84b639ccb6023f926eb78
PratyutCS <pratyut987@gmail.com>
2024-01-10 05:49:45 +0530

    3d changes

--- file.js
+++ file.js
@@ -6,6 +6,7 @@ function oh(i){
         document.querySelector(".qq2").classList.remove("active");
         document.querySelector(".q3").classList.remove("active");
         document.querySelector(".qq3").classList.remove("active");
+        document.querySelector(".oho").src = "/assests/Group 38.svg";
     }
     else if(i==2){
         document.querySelector(".q1").classList.remove("active");
@@ -14,6 +15,9 @@ function oh(i){
         document.querySelector(".qq2").classList.add("active");
         document.querySelector(".q3").classList.remove("active");
         document.querySelector(".qq3").classList.remove("active");
+        document.querySelector(".oho").src = "./assests/video-annotation-for-deep-learning.gif";
+        document.querySelector(".p1").innerHTML = "Boost Visual Intelligence! Explore our Video Annotation service for precise object detection and tracking across frames. Elevate your computer vision models with seamless accuracy and efficiency.";
+
     }
     else{
         document.querySelector(".q1").classList.remove("active");
@@ -22,6 +26,9 @@ function oh(i){
         document.querySelector(".qq2").classList.remove("active");
         document.querySelector(".q3").classList.add("active");
         document.querySelector(".qq3").classList.add("active");
+        document.querySelector(".oho").src = "/assests/se.svg";
+        document.querySelector(".p1").innerHTML = "Precision in Every Dimension: Elevate 3D Computer Vision Accuracy with Our Annotation Services. From Camera to Lidar and Radar, Unlock Enhanced Models with 3D Bounding Boxes, 2D-3D Linking, and Point Cloud Segmentations.";
+
     }
 }
 
@@ -59,11 +66,21 @@ window.addEventListener('scroll', (e) => {
   for(let i = 0; i < stickySections.length; i++){
     transform(stickySections[i])
   }
-  // console.log(document.documentElement.scrollTop);
-  if (document.documentElement.scrollTop >= 4350) {
+  // console.log(document.documentElement.scrollTop+" "+document.querySelector(".number").offsetTop+" "+(document.querySelector(".number").offsetTop - screen.height + 100));
+  if (document.documentElement.scrollTop >= (document.querySelector(".number").offsetTop - screen.height/1.5)){
     if (called) return
     called = true
     calledEvent()
+  }
+  if(document.documentElement.scrollTop <= (document.querySelector(".number").offsetTop - screen.height/1.5)){
+    if(!called){
+      return
+    }
+    called = false
+    const counters = document.querySelectorAll(".count");
+    counters.forEach((counter) => {
+        counter.innerText = 0;
+    });
   }
 })
 
@@ -86,11 +103,68 @@ function calledEvent() {
       const increment = Math.trunc(target / speed);
       if (count < target) {
         counter.innerText = count + increment;
-        setTimeout(updateCount, 20);
+        setTimeout(updateCount, 30);
       } else {
-        count.innerText = target;
+        counter.innerText = target;
       }
     };
     updateCount();
   });
+}
+
+let constrain = 20;
+let mouseOverContainer = document.getElementById("ex1");
+let ex1Layer = document.getElementById("ex1-layer");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(100vh)"
+    + "rotateX("+ calcX +"deg)"
+    + "rotateY("+ calcY +"deg)";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
+
+function butth(i){
+  if(i==1){
+    document.querySelector(".butth1").classList.add("butth-active");
+    document.querySelector(".butth2").classList.remove("butth-active");
+    document.querySelector(".butth3").classList.remove("butth-active");
+    document.querySelector(".butth4").classList.remove("butth-active");
+    document.querySelector(".butthimg").src = "./assests/Group 39.svg";
+  }
+  else if(i==2){
+    document.querySelector(".butth1").classList.remove("butth-active");
+    document.querySelector(".butth2").classList.add("butth-active");
+    document.querySelector(".butth3").classList.remove("butth-active");
+    document.querySelector(".butth4").classList.remove("butth-active");
+    document.querySelector(".butthimg").src = "./assests/bbc.gif";
+  }
+  else if(i==3){
+    document.querySelector(".butth1").classList.remove("butth-active");
+    document.querySelector(".butth2").classList.remove("butth-active");
+    document.querySelector(".butth3").classList.add("butth-active");
+    document.querySelector(".butth4").classList.remove("butth-active");
+  }
+  else{
+    document.querySelector(".butth1").classList.remove("butth-active");
+    document.querySelector(".butth2").classList.remove("butth-active");
+    document.querySelector(".butth3").classList.remove("butth-active");
+    document.querySelector(".butth4").classList.add("butth-active");
+    document.querySelector(".butthimg").src = "./assests/Recipet.svg";
+  }
 }
